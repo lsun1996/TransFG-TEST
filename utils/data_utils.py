2f44176c25a22fa3002bd857dcb48681458c2a64
lsun1996 <123316243+lsun1996@users.noreply.github.com>
2024-05-27 17:40:51 -0400

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -7,7 +7,7 @@ import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
 
-from .dataset import CUB, CarsDataset, NABirds, dogs, INat2017
+from .dataset import CUB
 from .autoaugment import AutoAugImageNetPolicy
 
 logger = logging.getLogger(__name__)
